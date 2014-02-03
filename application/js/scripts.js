8dbb610c83682d051599eaa8d57019e0fda2d906
nicolasverlhiac <nicolas.verlhiac@gmail.com>
2014-02-02 22:47:17 -0500

    ADD - function JS

---
+++ application/js/scripts.js
@@ -0,0 +1,17 @@
+// var secondes = 0;
+// var minutes = 0;
+
+// setInterval(function () {
+// 	if (secondes == 60) {
+// 		minutes ++;
+// 		secondes = 0;
+// 	};
+
+// 	if (minutes < 10) {
+// 		document.body.innerHTML = '0' + minutes + ' : ' + secondes++;
+// 	};
+
+// 	if (secondes < 10) {
+// 		document.body.innerHTML += '0' + secondes++;
+// 	};
+// },1000);
